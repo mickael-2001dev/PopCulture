a226cf601b6d0c59713eaf8b8f8d60322ca385c5
Mickael Souza <mickael.souza.if@gmail.com>
2018-07-17 00:34:15 -0300

    Alterações no admin, login com ajax

--- controllers/Admin.php
+++ controllers/Admin.php
@@ -1,5 +1,203 @@
 <?php 
+class Admin extends Controller
+{
 
+    protected $login;
+    private $user;
+    private $imagem;
+
+    public function __construct() 
+    {
+        parent::__construct();
+        $this->view->setTemplate('admin');
+        $this->login = new Login();
+        $this->model = new UserModel();
+        $this->imagem = new ImagemModel();
+  
+
+        if(!$this->login->isLogged()) {
+            $this->doLogin();
+            die;
+        }
+     
+   
+
+    }
+        
+    public function index()
+    {
+        $this->view->load('header');
+        $this->view->load('nav');
+        $this->view->load('index');
+        $this->view->load('footer');
+    } 
+
+    public function login() {
+        $this->view->load('login2');
+
+      
+    }
+
+    public function doLogin()
+    {
+       
+
+        if($_POST) {
+            $indexes = Form::indexInput($_POST);
+
+            $this->verifySessionCaptcha($indexes);
+              
+           
+            if($this->login->verifyLogin(new User($indexes['user'], $indexes['pass']))) {
+
+                // if($indexes['continue']) {
+                    // $this->login->createCookies();
+                // }
+
+                $this->login->createSession();
+
+                $this->verifyThePasswords($indexes);
+
+                print $this->login->isLogged();        
+            } else {
+                Message::error('Usuário ou senha incorretos!');
+            }
+
+        } else {
+            $numbers = $this->numbersGenerator(1, 20);
+            Session::createSessionByArray($numbers);
+            $this->login();
+        }
+
+       
+       
+        
+    }
+
+    public function logout()
+    {
+        $this->login->logout();
+        header('location:' . $this->config->base_url . 'home');
+    }  
+
+    
+    public function changePassword($id) 
+    {
+    
+        $data = null;  
+        $work = null;
+        //$this->login->logout();
+
+        $user = $this->model->getUserById($id);
+       
+        $user = $user->getUsername();
+      
+
+        if(filter_input(INPUT_POST,'update')) {
+            //$this->view->load('update-password', $data);  
+            
+           
+
+
+            $indexes = Form::indexInput($_POST);
+            if($indexes['new-pass'] == $indexes['repeat-pass']){
+
+                $validatePassUser = new User($user, $indexes['new-pass']);
+
+                if($validatePassUser->validatePassword()){
+                    if($this->login->updatePassword($user, $indexes['new-pass'])) {
+                        $this->location();
+                    } else {
+                        $data['msg'] = "Não foi possivel atualizar a senha!";
+                    }
+                } else {
+                    $data['msg'] = "A senha precisa possuir 4 letras e 4 números!"; 
+                }
+            } else {
+               $data['msg'] = "Senhas não conferem!";
+            }
+        }
+
+        if(!$work) {
+            $this->view->load('update-password', $data);
+        } 
+        
+    }
+    
+    private function numbersGenerator($begin, $end) 
+    {
+        $number1 = rand($begin, $end);
+        $number2 = rand($begin, $end);
+        $soma = $number1 + $number2;
+
+        $numbers = [
+            'num1'=>$number1,
+            'num2'=>$number2,
+            'soma'=>$soma
+        ];
+
+        return $numbers;
+    }
+
+
+    private function verifySessionCaptcha($indexes)
+    {
+        if($indexes['soma'] != Session::getSession('soma')){
+            $numbers = $this->numbersGenerator(1,20);
+            //Session::createSessionByArray($numbers);
+            Message::error('Soma incorreta, calcule novamente');
+            die;      
+        }
+    }
+
+    private function verifyThePasswords($indexes) 
+    {
+
+
+        if($this->login->verifyPassword($indexes['user'])) {
+                           
+            $user =  $this->model->getUserByLogin($indexes['user']);
+      
+            print $user->getId();
+            die;
+        }
+
+        if($this->login->verifyNewPassword($indexes['user'], $indexes['pass'])){
+
+            $user =  $this->model->getUserByLogin($indexes['user']);
+        
+            print $user->getId();
+            die;
+                                
+        }
+    }
+
+    
+  
+    protected function saveImagem($image)
+    {
+        $types = ['image/jpeg', 'image/png', 'image/gif'];
+
+        $insert = true;
+        $upload = new Image($image, $types);
+
+        if($upload->saveImage() !== true) {
+            $insert = false;
+            $data['msg'] = $upload->saveImage();
+            $this->error($data); 
+        }
+
+        if($insert) {
+            if($this->imagem->insert($upload->getName())){
+                return true;
+            }
+        } 
+
+    }
+
+}
+
+/*
 class Admin extends Controller
 {
 
@@ -97,7 +295,7 @@ class Admin extends Controller
         if($this->login->isLogged()) {
             $this->reload();
         } else {
-            $this->view->load('login', $data);
+            $this->view->load('login2', $data);
         }
        
         
@@ -194,55 +392,64 @@ class Admin extends Controller
 
     }
 
-    /*protected function error(array $data){
-        $this->view->load('error', $data);
-    } 
-
-    protected function success(array $data){
-        $this->view->load('success', $data);
-    }*/
-
-    /*protected function verifyInputIndexes($indexes) 
-    {
-        $fieldIsOk = true;
-
-        foreach ($indexes as $key => $value) {
-            if($value === "") {
-                $fieldIsOk = false;
-            }
-        }
-
-        if(!$fieldIsOk) {
-            Message::error("Campos Vazios!");
-            return false;
-            die;
-        }
-
-        return true;
-    }*/
-
-}
+    */
 
 
- /*$type_image = false;
-        $dir = 'views/img/'.$image['name'];
-        //var_dump($image);
-        foreach($types as $type){
-            if($image['type'] == $type){
-               $type_image = true;
-            }
-        }
-        //var_dump($dir);
-        if($type_image){
-            if(move_uploaded_file($image['tmp_name'], $dir)){
-                if($this->imagem->insert($image['name'])){
-                    return true;
-                }
-            } else {
-                $data['msg'] = "Não foi possível realizar o upload de imagem!";
-                $this->error($data);
-            } 
-        } else {
-            $data['msg'] = "Extensão não suportada!";
-            $this->error($data); 
-        }*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
